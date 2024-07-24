121336090351531d43c815f1c8317e6a733e20b8
Fabrizio Rossier <fabriziorossier@gmail.com>
2024-07-23 20:07:32 -0400

    feat: added responsive product grid

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { MatCardModule } from '@angular/material/card';
-
+import { FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -11,6 +11,7 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     MatCardModule,
+    FlexModule,
   ]
 })
 export class ProductModule { }
