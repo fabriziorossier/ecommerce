63b834d2e56a37e1762ab69312a835c0888e1730
Fabrizio Rossier <fabriziorossier@gmail.com>
2024-07-27 10:17:09 -0400

    feat: displaying cartItems

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
@@ -8,7 +8,7 @@ import { Product } from '../models/product';
   providedIn: 'root'
 })
 export class CartService {
-  private apiUrl = environment.apiUrl + " /cart";
+  private apiUrl = environment.apiUrl + "/cart";
 
   constructor(private http: HttpClient) { }
 
